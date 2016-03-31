08fcab0e2ff07efc785c3d2ff8ff493e488ffaa3
Martina Kraus <wing121@web.de>
2016-03-31 14:09:03 +0200

    :lipstick: style fixes

--- client/app/contact/contact-controller.js
+++ client/app/contact/contact-controller.js
@@ -1,15 +1,11 @@
 angular.module('app')
   .controller('contactCtrl', ['$scope', function ($scope) {
-    // here we define the items to be repeated in the template
-    $scope.list1 = [
-      {label: 'one'},
-      {label: 'two'},
-      {label: 'three'}
-    ];
- 
-    $scope.list2 = [
-      {label: 'uno'},
-      {label: 'dos'},
-      {label: 'tres'}
-    ];
-  }]);
+    $scope.submitForm = function() {
+        if ($scope.testForm.$valid) {
+            alert('Form valid!');
+        }
+        else {
+            alert('Form not valid!');
+        }
+    };
+});
